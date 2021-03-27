bff15cef6e2f3e59bd2f628b47150d20e1b08e0d
KarWad <phinb@LAPTOP-25LHGEP6.(none)>
2021-03-27 18:13:45 +0100

    Dodaj pliki projektów.

---
+++ Singleton/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Bike
+    {
+        private Bike()
+        {
+
+        }
+        public static Bike obj;
+        public static Bike GetInstance()
+        {
+            if (obj == null)
+            {
+                obj = new Bike();
+            }
+            return obj;
+        }
+        public string getDetails()
+        {
+            return "Winora";
+        }
+        static void Main(string[] args)
+        {
+            Bike bike = Bike.GetInstance();
+            string x = bike.getDetails();
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
